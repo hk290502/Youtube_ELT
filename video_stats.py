9c24406fd252b17588a9edfb60ae5e412c06de13
Hamza Khan <hamza.khan@envitia.com>
2026-04-23 16:48:46 +0100

    .env

--- video_stats.py
+++ video_stats.py
@@ -6,9 +6,11 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path="./.env")
 
-API_KEY = os.getenv("API_KEY")
+API_KEY = os.getenv("API_K")
 
 CHANNEL_HANDLE = "MrBeast"
+
+maxResults = 50
 
 def get_playlistId():
   
@@ -36,6 +38,44 @@ def get_playlistId():
 
   except requests.exceptions.RequestException as e:
     raise e
-  
+
+
+def get_video_ids(playlisId):
+   
+   video_ids = []
+
+   pageToken = None
+   
+   base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=ContentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
+
+   try:
+      
+      while True:
+         url = base_url
+
+         if pageToken:
+            url += f"pageToken={pageToken}"
+
+         response = requests.get(url)
+
+         response.raise_for_status()
+
+         data = response.json()
+
+         for item in data.get("items", []):
+            video_id = item["contentDetails"]["videoId]"]
+            video_ids.append(video_id)
+
+         pageToken = data.get('nextPageToken')
+
+         if not pageToken:
+             break
+         
+      return video_ids
+
+      
+   except requests.exceptions.RequestException as e:
+      raise e
+
 if __name__ == "__main__":
-    get_playlistId()
+    playlistId = get_playlistId()
